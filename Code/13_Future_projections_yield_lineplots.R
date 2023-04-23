5f2c9071d4ac57bf32b28dbfaba9b04cb31dcc8e
Sam Majumder <sammajumder692@gmail.com>
2023-04-23 17:53:34 -0400

    Third commit(4/23/2023)

--- Code/13_Future_projections_yield_lineplots.R
+++ Code/13_Future_projections_yield_lineplots.R
@@ -11,25 +11,53 @@ Future_predictions <- readRDS(here("RDS_objects",
                                    "Future_predictions.RDS")) %>% 
                                                           st_drop_geometry()
                                                   
-Average_National_Yield <- Future_predictions %>% 
-                             group_by(RCP,Timeframe) %>% 
-                           summarize(Average_National_Yield = mean(Predictions))
+Average_Yearly_Yield <- Future_predictions %>% 
+                             group_by(State,RCP,Timeframe) %>% 
+                           summarize(Average_Yearly_Yield = mean(Predictions))
 
 
-Average_National_Error <- Future_predictions %>% 
-                                       group_by(RCP,Timeframe) %>% 
-                           summarize(Average_National_Error = mean(Mean_Error))
+#Average_National_Error <- Future_predictions %>% group_by(RCP,Timeframe) %>% summarize(Average_National_Error = mean(Mean_Error))
                            
 
 
-Average_National_Yield <- Average_National_Yield %>% 
-                                            inner_join(Average_National_Error)
+#Average_National_Yield <- Average_National_Yield %>% inner_join(Average_National_Error)
+
+Average_Yearly_Yield <- Average_Yearly_Yield %>% 
+                                   mutate(State = case_when(str_detect(State,
+                                                                       "CO") ~ 
+                                                                   "Colorado",
+                                                             str_detect(State,
+                                                                        "KS") ~ 
+                                                                      "Kansas",
+                                                              str_detect(State,
+                                                                         "MN") ~ 
+                                                                   "Minnesota",
+                                                               str_detect(State,
+                                                                     "ND") ~ 
+                                                                "North Dakota",
+                                                               str_detect(State,
+                                                                     "NE") ~
+                                                                    "Nebraska",
+                                                              str_detect(State,
+                                                                        "SD") ~
+                                                                "South Dakota",
+                                                              str_detect(State,
+                                                                        "TX") ~
+                                                                     "Texas"))
+
+
 
 
 ###### Setting the factors ### for RCPS
 
-Average_National_Yield$RCP <- factor(Average_National_Yield$RCP, 
-                                     levels = c("126","245","370","585"))
+Average_Yearly_Yield$State <- factor(Average_Yearly_Yield$State, 
+                                     levels = c("South Dakota",
+                                                "Nebraska",
+                                                "Texas",
+                                                "North Dakota",
+                                                "Colorado",
+                                                "Minnesota",
+                                                "Kansas"))
 
 ######## Display color pallette ##
 
@@ -38,27 +66,31 @@ Average_National_Yield$RCP <- factor(Average_National_Yield$RCP,
 
 display.brewer.all(colorblindFriendly = T) ### color friendly palettes
 
-display.brewer.pal(9,'YlOrRd')
+display.brewer.pal(8,'Set2')
 
 
-brewer.pal(9, 'YlOrRd') ## displaying the hex codes 
+brewer.pal(8, 'Set2') ## displaying the hex codes 
 
 
 
-Average_National_Yield %>% 
+Average_Yearly_Yield %>% 
   ggplot(aes(x= Timeframe,
-        y=Average_National_Yield,
-        group=RCP,
-        color=RCP)) +
-  geom_line()  +
+        y=Average_Yearly_Yield,
+        group=State,
+        colour = State)) +
+  geom_line(size = 1) +
   geom_point() +
+  scale_color_manual("Regions",
+                    values = c("#FC8D62","#8DA0CB","#E78AC3","#A6D854",
+                               "#FFD92F","#E5C494","#B3B3B3")) +
   facet_wrap(~RCP) +
   labs(x = "Timeframe",
-       y="Average National Yield") +
-  theme(text = element_text(size = 10)) 
+       y="Average Yearly Yield")  +
+  theme(text = element_text(size = 10)) +
+  theme_bw()
 
 
-ggsave("Figure 4.svg")
+ggsave("Figure 5.svg")
 
 
 

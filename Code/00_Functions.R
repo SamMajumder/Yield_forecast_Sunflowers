dea5cdfbeec6d2385f33f173460974f634a8ae3d
SamMajumder <sammajumder692@gmail.com>
2023-11-02 01:33:47 -0400

    New updates

--- Code/00_Functions.R
+++ Code/00_Functions.R
@@ -926,14 +926,15 @@ geoRflow_raster_pipeline_point <- function(inputs,
   }
   
   # Helper function to load and assign CRS to a raster file
-  load_raster <- function(input) {
+  load_raster <- function(input, method = "terra", crs = "EPSG:4326") {
     cat("Loading raster:", input, "\n")
+    
     raster_data <- tryCatch({
       if (is.character(input)) {
         if (method == "stars") {
           stars::read_stars(input)
         } else {
-          raster::brick(input)
+          terra::rast(input)
         }
       } else {
         input
@@ -943,28 +944,62 @@ geoRflow_raster_pipeline_point <- function(inputs,
       stop(e)
     })
     
-    # Assign user-specified CRS
-    cat("Transforming CRS to raster:", input, "\n")
-    if (method == "stars") {
-      raster_data <- sf::st_transform(raster_data, crs)
+    # Ensure the crs argument is a character string for terra::project()
+    crs_char <- if(is.list(crs)) {
+      st_as_text(crs)
+    } else if(is.character(crs)) {
+      crs
     } else {
-      raster::crs(raster_data) <- crs
+      stop("The CRS must be specified as a character string or as an sf CRS object.")
     }
     
-    return(raster_data)
+    cat("Reprojecting raster to user-defined CRS:", crs_char, "\n")
+    
+    reprojected_raster <- tryCatch({
+      if (method == "stars") {
+        sf::st_transform(raster_data, crs)
+      } else {
+        terra::project(raster_data, crs_char)
+      }
+    }, error = function(e) {
+      cat("Error in reprojecting raster.\n")
+      stop(e)
+    })
+    
+    return(reprojected_raster)
   }
   
   # Helper function to resample a raster
-  resample_raster <- function(raster_data) {
+  resample_raster <- function(raster_data, 
+                              method = "terra",
+                              resample_factor = NULL, 
+                              method_resampling = "bilinear", 
+                              no_data_value = -9999) {
+    
     cat("Resampling raster...\n")
     if (!is.null(resample_factor)) {
-      new_dims <- purrr::map_dbl(dim(raster_data), ~ round(.x * resample_factor))
       resampled_raster <- tryCatch({
-        stars::st_warp(raster_data,
-                       dimensions = new_dims,
-                       method = method_resampling,
-                       use_gdal = TRUE,
-                       no_data_value = no_data_value)
+        if (method == "terra") {
+          # Get the current resolution
+          old_res <- terra::res(raster_data)
+          # Calculate the new resolution based on the resample_factor
+          new_res <- old_res / resample_factor
+          # Create a reference raster with the new resolution
+          reference_raster <- terra::rast(raster_data)
+          reference_raster <- terra::resample(reference_raster, raster_data, method = method_resampling)
+          # Resample the original raster to match the reference raster
+          resampled_raster <- terra::resample(raster_data, reference_raster, method = method_resampling)
+        } else if (method == "stars") {
+          # For stars: use st_warp to resample
+          new_dims <- purrr::map_dbl(dim(raster_data), ~ round(.x * resample_factor))
+          stars::st_warp(raster_data,
+                         dimensions = new_dims,
+                         method = method_resampling,
+                         use_gdal = TRUE,
+                         no_data_value = no_data_value)
+        } else {
+          stop("Unknown method: ", method)
+        }
       }, error = function(e) {
         cat("Error in resampling raster.\n")
         stop(e)
@@ -976,39 +1011,82 @@ geoRflow_raster_pipeline_point <- function(inputs,
   }
   
   # Helper function to crop a raster to a reference shape
-  crop_raster <- function(raster_data) {
+  crop_raster <- function(raster_data, 
+                          method = "terra",
+                          reference_shape = NULL) {
+    
     cat("Cropping raster...\n")
     if (!is.null(reference_shape)) {
-      ref_shape <- tryCatch({
-        if (is.character(reference_shape)) {
-          sf::st_read(reference_shape, quiet = TRUE)
+      cropped_raster <- tryCatch({
+        if (method == "terra") {
+          # For terra: use terra::crop
+          ref_shape <- if (is.character(reference_shape)) {
+            sf::st_read(reference_shape, quiet = TRUE)
+          } else {
+            reference_shape
+          }
+          ref_shape <- sf::st_transform(ref_shape, sf::st_crs(raster_data))
+          terra::crop(raster_data, terra::vect(ref_shape))
+        } else if (method == "stars") {
+          # For stars: use sf::st_crop
+          ref_shape <- if (is.character(reference_shape)) {
+            sf::st_read(reference_shape, quiet = TRUE)
+          } else {
+            reference_shape
+          }
+          ref_shape <- sf::st_transform(ref_shape, sf::st_crs(raster_data))
+          suppressWarnings(sf::st_crop(raster_data, ref_shape))
         } else {
-          reference_shape
+          stop("Unknown method: ", method)
         }
       }, error = function(e) {
-        cat("Error reading reference shape.\n")
+        cat("Error cropping raster.\n")
         stop(e)
       })
-      ref_shape <- sf::st_transform(ref_shape, sf::st_crs(raster_data))
-      cropped_raster <- suppressWarnings(sf::st_crop(raster_data, ref_shape))
       return(cropped_raster)
     } else {
       return(raster_data)
     }
   }
-  
   # Helper function to extract raster values at specified point locations
-  extract_raster_values <- function(raster_data, df_sf) {
+  extract_raster_values <- function(raster_data, 
+                                    df,
+                                    lon_col,
+                                    lat_col,
+                                    method = "terra",
+                                    use_bilinear = TRUE) {
     cat("Extracting raster values...\n")
+    
+    # Convert the data frame to an sf object
+    df_sf <- tryCatch({
+      sf::st_as_sf(df, coords = c(lon_col, lat_col), crs = sf::st_crs(raster_data))
+    }, error = function(e) {
+      cat("Error in converting dataframe to sf object:\n", conditionMessage(e), "\n")
+      return(NULL)
+    })
+    
     extracted_values <- tryCatch({
-      stars::st_extract(raster_data, df_sf, bilinear = use_bilinear)
+      if (method == "terra") {
+        # For terra: use terra::extract
+        terra::extract(raster_data, terra::vect(df_sf), method = ifelse(use_bilinear, "bilinear", "simple"))
+      } else if (method == "stars") {
+        # Ensure raster_data is a stars object
+        if (!inherits(raster_data, "stars")) {
+          raster_data <- stars::read_stars(raster_data)
+        }
+        # For stars: use stars::st_extract without bilinear argument
+        stars::st_extract(raster_data, df_sf)
+      } else {
+        stop("Unknown method: ", method)
+      }
     }, error = function(e) {
       cat("Error in extracting raster values:\n", conditionMessage(e), "\n")
       return(NULL)
     })
-    return(extracted_values[[1]])  # This line should be outside of the tryCatch block
+    return(extracted_values)
   }
   
+
   # Process a single data frame (either the whole df or a subset)
   process_df <- function(current_df, files, file_names) {
     for (i in seq_along(files)) {
@@ -1026,12 +1104,13 @@ geoRflow_raster_pipeline_point <- function(inputs,
     return(current_df)
   }
   
+  
   # Initialize the progress bar
   pb <- txtProgressBar(min = 0, max = length(inputs), style = 3)
   
   # Main processing function to load, resample, and crop rasters
   process_single_raster <- function(input, index) {
-    raster_data <- load_raster(input)
+    raster_data <- load_raster(input,crs = crs)
     raster_data <- resample_raster(raster_data)
     raster_data <- crop_raster(raster_data)
     # Update the progress bar
@@ -1083,7 +1162,63 @@ geoRflow_raster_pipeline_point <- function(inputs,
   return(list(processed_rasters = processed_rasters, dataframes_with_values = dataframes_with_values))
 }
 
+################ 
+#### A function to extract the elements from the result of the geoRflow_raster_pipeline_point
 
+#### function ##
+process_dataframes <- function(temp_list, element_name, new_col_names, extract_index = 1) {
+  # Check if the element exists in the list
+  if (!element_name %in% names(temp_list)) {
+    stop("The specified element does not exist in the provided list.")
+  }
+  
+  # Extract the specified element (assumed to be a list of data frames)
+  dataframes_with_values <- temp_list[[element_name]]
+  
+  # Initialize an empty list for the processed data frames
+  processed_dataframes <- list()
+  
+  # Process each dataframe
+  for (i in seq_along(dataframes_with_values)) {
+    df <- dataframes_with_values[[i]]
+    
+    # Create a new dataframe for processed columns
+    new_df <- data.frame(matrix(ncol = 0, nrow = nrow(df)))
+    
+    # Process each column
+    for (col_name in names(df)) {
+      if (is.data.frame(df[[col_name]]) || is.matrix(df[[col_name]])) {
+        # Extract the part that doesn't contain "ID" and convert to a list
+        sub_df <- df[[col_name]][, !grepl("ID", names(df[[col_name]]))]
+        new_df[[col_name]] <- as.list(sub_df)
+      } else {
+        # Keep the column as is
+        new_df[[col_name]] <- df[[col_name]]
+      }
+    }
+    
+    # Flatten list-columns
+    list_cols <- sapply(new_df, is.list)
+    if (any(list_cols)) {
+      new_df[list_cols] <- lapply(new_df[list_cols], function(x) sapply(x, function(y) y[[extract_index]]))
+    }
+    
+    # Rename the columns of the new dataframe
+    if (length(new_col_names) == ncol(new_df)) {
+      colnames(new_df) <- new_col_names
+    } else {
+      warning("The number of new column names does not match the number of columns in the dataframe.")
+    }
+    
+    # Add the new dataframe to the list of processed dataframes
+    processed_dataframes[[i]] <- new_df
+  }
+  
+  # Combine all data frames in 'processed_dataframes' into one large data frame
+  final_dataframe <- do.call(rbind, processed_dataframes)
+  
+  return(final_dataframe)
+}
 
 
 
--- Code/02_HistoricalClimateDataProcess.R
+++ Code/02_HistoricalClimateDataProcess.R
@@ -2,7 +2,8 @@
 rm(list = ls())
 #### Packages ###
 
-packages <- list("here","tidyverse","ggplot2","sf","raster","stars","ncdf4")
+packages <- list("here","tidyverse","ggplot2","sf","raster","stars","ncdf4",
+                 "stringr")
 lapply(packages, require,character.only = T)
 
 
@@ -93,6 +94,8 @@ layer_indices <- 973:1536
 # Specify the path to the 'HistoricalTempMax' folder
 # Define the path to the HistoricalTempMax folder within the RawDatasets folder
 output_dir <- here("RawDatasets", "HistoricalTempMax")
+output_dir_2 <- here("RawDatasets", "HistoricalTempMin")
+output_dir_3 <- here("RawDatasets", "HistoricalPrcp")
 
 
 ### export the layers from the netcdf files (temp max)
@@ -103,14 +106,26 @@ export_layers(raster_brick = temp_data_max,
               output_dir = output_dir)
 
 
+export_layers(raster_brick = temp_data_min,
+              layer_indices = layer_indices,
+              output_dir = output_dir_2) 
+
+export_layers(raster_brick = prcp_data_total,
+              layer_indices = layer_indices,
+              output_dir = output_dir_3)
+
+
 
 
 
 ### Read in the Sunflower Yield shapefile 
+## and transforming the datasets to get accurate coordinates for this project ##
 
 
 Sunflower_Yield <- st_read(here("Shapefiles","SunflowerYield",
-                                "SunflowerYield.shp"))
+                                "SunflowerYield.shp")) 
+
+
 
 ### Extract the coordinates from the file ###
 
@@ -120,12 +135,12 @@ Coordinates <- Sunflower_Yield %>%
                     dplyr::filter(YEAR >= 1976 & YEAR <= 2020)
 
 
-
+########### TMAX RASTERS #####
 
 ### use these coordinates to extract data at points from all rasters ###
 
 ## listing all tmax files 
-inputs <- list.files(
+rasters <- list.files(
   path = here("RawDatasets", "HistoricalTempMax"),
   pattern = ".*\\.tif$",
   full.names = TRUE
@@ -133,42 +148,24 @@ inputs <- list.files(
 
 # Create a vector of search strings based on the unique years in your dataframe
 
+
 unique_years <- unique(Coordinates$YEAR)
 
 search_strings <- paste0(unique_years)
 
-
-
 #########################################################
 
 
-# Set up the call to raster_pipeline_point
-temp_max_list <- geoRflow_raster_pipeline_point(inputs = inputs,
-                                          df = Coordinates,
-                                          lat_col = "LAT",
-                                          lon_col = "LON",
-                                          split_id = "YEAR",
-                                          search_strings = search_strings,
-                                          method = "stars",
-                                          resample_factor = NULL,
-                                          crs = st_crs(9822),
-                                          method_resampling = "bilinear",
-                                          no_data_value = -9999,
-                                          reference_shape = NULL,
-                                          use_bilinear = TRUE)
-
-
-
-
-temp_max_list_2 <- geoRflow_raster_pipeline_point(inputs = inputs,
+# EXTRACT VALUES AT THE COORDINATES 
+temp_max_list <- geoRflow_raster_pipeline_point(inputs  = rasters,
                                                 df = Coordinates,
                                                 lat_col = "LAT",
                                                 lon_col = "LON",
                                                 split_id = "YEAR",
                                                 search_strings = search_strings,
-                                                method = "stars",
+                                                method = "terra",
                                                 resample_factor = NULL,
-                                                crs = st_crs(4735),
+                                                crs = "EPSG:4326",
                                                 method_resampling = "bilinear",
                                                 no_data_value = -9999,
                                                 reference_shape = NULL,
@@ -176,48 +173,168 @@ temp_max_list_2 <- geoRflow_raster_pipeline_point(inputs = inputs,
 
 
 
+################ 
+#### Extracting the list that contains the dataframes with values ###
+
+# Define the new column names
+new_column_names <- c("YEAR", "STATE", "COUNTYN", "LON", "LAT", 
+                      "Jan_tmax", "Feb_tmax", "Mar_tmax", "Apr_tmax", 
+                      "May_tmax", "June_tmax", "July_tmax", "August_tmax", 
+                      "Sept_tmax", "Oct_tmax", "Nov_tmax", "Dec_tmax")
+
+
+Max_temp <- process_dataframes(temp_max_list,
+                               "dataframes_with_values",
+                   new_col_names = new_column_names,
+                   extract_index = 1)
 
 
 
 
+############ TMIN ###########
 
 ####### 
+### use these coordinates to extract data at points from all rasters ###
 
-
-raster_files_tmin <- list.files(path = here("RawDatasets", "HistoricalTempMin"), 
-                                full.names = TRUE)
-
-result <- geoRflow_raster_pipeline_point(
-  inputs = inputs,  # replace with your list of raster file paths
-  df = Coordinates,
-  lat_col = "LAT",
-  lon_col = "LON",
-  split_id = "YEAR",
-  search_strings = search_string_1976_1977,
-  method = "stars",
-  resample_factor = NULL,  # or specify a resample factor if needed
-  crs = st_crs(4326),
-  method_resampling = "bilinear",
-  no_data_value = -9999,
-  reference_shape = NULL,  # or specify a reference shape if needed
-  use_bilinear = TRUE
+## listing all tmax files 
+rasters_2 <- list.files(
+  path = here("RawDatasets", "HistoricalTempMin"),
+  pattern = ".*\\.tif$",
+  full.names = TRUE
 )
 
+# Create a vector of search strings based on the unique years in your dataframe
 
 
-##### Listing all prcp files ##### 
+unique_years <- unique(Coordinates$YEAR)
 
-raster_files_prcp <- list.files(path = here("RawDatasets", "HistoricalPrcp"), 
-                                full.names = TRUE)
+search_strings <- paste0(unique_years)
 
-
-
-#### Join the four datasets ###
-### Tmax, Tmin, Precip, Yield 
+#########################################################
 
 
+# Set up the call to raster_pipeline_point
+temp_min_list <- geoRflow_raster_pipeline_point(inputs  = rasters_2,
+                                                df = Coordinates,
+                                                lat_col = "LAT",
+                                                lon_col = "LON",
+                                                split_id = "YEAR",
+                                                search_strings = search_strings,
+                                                method = "terra",
+                                                resample_factor = NULL,
+                                                crs = "EPSG:4326",
+                                                method_resampling = "bilinear",
+                                                no_data_value = -9999,
+                                                reference_shape = NULL,
+                                                use_bilinear = TRUE)
 
 
+
+################ 
+#### Extracting the list that contains the dataframes with values ###
+
+# Define the new column names
+new_column_names <- c("YEAR", "STATE", "COUNTYN", "LON", "LAT", 
+                      "Jan_tmin", "Feb_tmin", "Mar_tmin", "Apr_tmin", 
+                      "May_tmin", "June_tmin", "July_tmin", "August_tmin", 
+                      "Sept_tmin", "Oct_tmin", "Nov_tmin", "Dec_tmin")
+
+
+Min_temp <- process_dataframes(temp_min_list,
+                               "dataframes_with_values",
+                               new_col_names = new_column_names,
+                               extract_index = 1)
+
+
+
+#####################
+#### PRCP #############
+############# 
+
+####### 
+### use these coordinates to extract data at points from all rasters ###
+
+## listing all tmax files 
+rasters_3 <- list.files(
+  path = here("RawDatasets", "HistoricalPrcp"),
+  pattern = ".*\\.tif$",
+  full.names = TRUE
+)
+
+# Create a vector of search strings based on the unique years in your dataframe
+
+
+unique_years <- unique(Coordinates$YEAR)
+
+search_strings <- paste0(unique_years)
+
+#########################################################
+
+
+# Set up the call to raster_pipeline_point
+prcp_list <- geoRflow_raster_pipeline_point(inputs  = rasters_3,
+                                                df = Coordinates,
+                                                lat_col = "LAT",
+                                                lon_col = "LON",
+                                                split_id = "YEAR",
+                                                search_strings = search_strings,
+                                                method = "terra",
+                                                resample_factor = NULL,
+                                                crs = "EPSG:4326",
+                                                method_resampling = "bilinear",
+                                                no_data_value = -9999,
+                                                reference_shape = NULL,
+                                                use_bilinear = TRUE)
+
+
+
+################ 
+#### Extracting the list that contains the dataframes with values ###
+
+# Define the new column names
+new_column_names <- c("YEAR", "STATE", "COUNTYN", "LON", "LAT", 
+                      "Jan_prcp", "Feb_prcp", "Mar_prcp", "Apr_prcp", 
+                      "May_prcp", "June_prcp", "July_prcp", "August_prcp", 
+                      "Sept_prcp", "Oct_prcp", "Nov_prcp", "Dec_prcp")
+
+
+Total_prcp <- process_dataframes(prcp_list,
+                               "dataframes_with_values",
+                               new_col_names = new_column_names,
+                               extract_index = 1)
+
+
+
+#######
+### Joining the climate data into one dataframe ####
+### 
+
+Climate_data <- list(Max_temp,Min_temp,Total_prcp) %>%
+                                      purrr::reduce(inner_join)
+
+
+
+###### Now adding the climate data to the Yield data ###
+
+Yield <- Sunflower_Yield %>% 
+                      sf::st_drop_geometry() %>%
+                      dplyr::filter(YEAR >= 1976 & YEAR <=2020) %>%
+                      dplyr::inner_join(Climate_data)
+
+
+
+########
+### writing out this dataset ###
+
+### Creating an output folder ###
+
+ProcessedDatasets <- here("ProcessedDatasets")
+
+dir.create(ProcessedDatasets, showWarnings = FALSE) 
+
+path <- here("ProcessedDatasets","HistoricalYieldClimateData.csv")
+
+write.csv(Yield,path,row.names = FALSE)
 
 
 
